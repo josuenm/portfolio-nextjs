3f27e7c5db55e1947ec0b8c2abed60410df07565
josuenm <direct.josue@gmail.com>
2022-09-15 14:01:01 -0300

    feat(new option added)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,114 +1,133 @@
-import { BackButton, Button, Container, LinksContainer, MobileButton, Navigation, ProjectHeaderContainer } from "./styles";
-import Logo from "src/assets/icons/Logo.svg";
 import Image from "next/image";
-import { SafeArea } from "../SafeArea";
-import { useContext, useEffect, useState } from "react";
-import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll'
 import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { animateScroll as scroll, Link as ScrollLink } from "react-scroll";
+import Logo from "src/assets/icons/Logo.svg";
 import { ContactContext, ContactContextType } from "src/contexts/contact";
-
+import { SafeArea } from "../SafeArea";
+import {
+  BackButton,
+  Button,
+  Container,
+  LinksContainer,
+  MobileButton,
+  Navigation,
+  ProjectHeaderContainer,
+} from "./styles";
 
 export function Header() {
-
   const { toggle } = useContext(ContactContext) as ContactContextType;
 
-    const [headerIsActive, setHeaderIsActive] = useState(false);
-    const [mobileButton, setMobileButton] = useState(false);
+  const [headerIsActive, setHeaderIsActive] = useState(false);
+  const [mobileButton, setMobileButton] = useState(false);
 
-    useEffect(() => {
-        const scrollListener = () => {
-          if (window.scrollY > 10) {
-            setHeaderIsActive(true);
-          } else {
-            setHeaderIsActive(false);
-          }
-        };
-    
-        window.addEventListener("scroll", scrollListener);
-        return () => {
-          window.removeEventListener("scroll", scrollListener);
-        };
-      }, []);
-
-    function handleMobileButton() {
-      if(!mobileButton) {
-        setHeaderIsActive(true)
-        setMobileButton(true)
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setHeaderIsActive(true);
       } else {
-        setMobileButton(false)
+        setHeaderIsActive(false);
       }
+    };
+
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
+  function handleMobileButton() {
+    if (!mobileButton) {
+      setHeaderIsActive(true);
+      setMobileButton(true);
+    } else {
+      setMobileButton(false);
     }
+  }
 
-    function handleTopScroll() {
-      setMobileButton(false)
-      scroll.scrollToTop();
-    }
+  function handleTopScroll() {
+    setMobileButton(false);
+    scroll.scrollToTop();
+  }
 
-    return (
-        <Container 
-          className={headerIsActive?'active':''}
-          data-mobile={mobileButton?true:false}>
-            <SafeArea>
-              <Navigation>
-                <Image 
-                  src={Logo} 
-                  layout="fixed" 
-                  alt="" 
-                  className="logo" 
-                  width={100} 
-                  height={50} 
-                  onClick={handleTopScroll} />
+  return (
+    <Container
+      className={headerIsActive ? "active" : ""}
+      data-mobile={mobileButton ? true : false}
+    >
+      <SafeArea>
+        <Navigation>
+          <Image
+            src={Logo}
+            layout="fixed"
+            alt=""
+            className="logo"
+            width={100}
+            height={50}
+            onClick={handleTopScroll}
+          />
 
-                <LinksContainer className={mobileButton?'active':''}>
-                  <ScrollLink 
-                    to="presentation"
-                    onClick={handleTopScroll}
-                    className="link">
-                      Inicio
-                  </ScrollLink>
+          <LinksContainer className={mobileButton ? "active" : ""}>
+            <ScrollLink
+              to="presentation"
+              onClick={handleTopScroll}
+              className="link"
+            >
+              Inicio
+            </ScrollLink>
 
-                  <ScrollLink 
-                    to="development" 
-                    className="link" 
-                    smooth={true} 
-                    offset={-100}
-                    onClick={() => setMobileButton(false)}>
-                      Desenvolvimento
-                  </ScrollLink>
+            <ScrollLink
+              to="aboutMe"
+              className="link"
+              smooth={true}
+              offset={-100}
+              onClick={() => setMobileButton(false)}
+            >
+              Sobre mim
+            </ScrollLink>
 
-                  <ScrollLink 
-                    to="my-network" 
-                    className="link" 
-                    smooth={true} 
-                    offset={-100}
-                    onClick={() => setMobileButton(false)}>
-                      Minha rede
-                  </ScrollLink>
+            <ScrollLink
+              to="development"
+              className="link"
+              smooth={true}
+              offset={-100}
+              onClick={() => setMobileButton(false)}
+            >
+              Desenvolvimento
+            </ScrollLink>
 
-                  <ScrollLink 
-                    to="projects" 
-                    className="link" 
-                    smooth={true} 
-                    offset={-100}
-                    onClick={() => setMobileButton(false)}>
-                      Projetos
-                  </ScrollLink>
-                  
-                  <Button onClick={() => toggle(true)}>Entrar em contato</Button>
-                </LinksContainer>
+            <ScrollLink
+              to="my-network"
+              className="link"
+              smooth={true}
+              offset={-100}
+              onClick={() => setMobileButton(false)}
+            >
+              Minha rede
+            </ScrollLink>
 
-                <MobileButton 
-                  className={mobileButton?'active':''} 
-                  onClick={handleMobileButton} />
+            <ScrollLink
+              to="projects"
+              className="link"
+              smooth={true}
+              offset={-100}
+              onClick={() => setMobileButton(false)}
+            >
+              Projetos
+            </ScrollLink>
 
-              </Navigation>
-          </SafeArea>
-      </Container>
-    )
+            <Button onClick={() => toggle(true)}>Entrar em contato</Button>
+          </LinksContainer>
+
+          <MobileButton
+            className={mobileButton ? "active" : ""}
+            onClick={handleMobileButton}
+          />
+        </Navigation>
+      </SafeArea>
+    </Container>
+  );
 }
-
-
-
 
 export function ProjectHeader({ href }: { href: string }) {
   return (
@@ -117,13 +136,14 @@ export function ProjectHeader({ href }: { href: string }) {
         <BackButton>Voltar</BackButton>
       </Link>
 
-      <Image 
-        src={Logo} 
-        layout="fixed" 
-        alt="" 
-        className="logo" 
-        width={100} 
-        height={50} />
+      <Image
+        src={Logo}
+        layout="fixed"
+        alt=""
+        className="logo"
+        width={100}
+        height={50}
+      />
     </ProjectHeaderContainer>
-  )
+  );
 }
