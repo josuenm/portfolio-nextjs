18e942c497dfa4dbf212be83073ecb9174a1df25
josuenm <direct.josue@gmail.com>
2022-11-12 20:18:59 -0300

    feat: test implemented

--- tests/components/SafeArea.spec.tsx
+++ tests/components/SafeArea.spec.tsx
@@ -1,22 +1,20 @@
-import { render } from "@testing-library/react";
 import "jest-styled-components";
 import renderer from "react-test-renderer";
-import { SafeArea } from "../../src/components/SafeArea";
 import { Container } from "../../src/components/SafeArea/styles";
 
 describe("SafeArea component", () => {
   it("has a maximum width of 1120px", () => {
-    const tree = renderer.create(<Container />).toJSON();
-    expect(tree).toHaveStyleRule("max-width", "1120px");
+    const element = renderer.create(<Container />).toJSON();
+    expect(element).toHaveStyleRule("max-width", "1120px");
   });
 
   it("has a 100% width", () => {
-    const tree = renderer.create(<Container />).toJSON();
-    expect(tree).toHaveStyleRule("width", "100%");
+    const element = renderer.create(<Container />).toJSON();
+    expect(element).toHaveStyleRule("width", "100%");
   });
 
   it("has auto horizontal margin", () => {
-    const element = render(<SafeArea />);
-    expect(element).toHaveStyleRule("width", "100%");
+    const element = renderer.create(<Container />).toJSON();
+    expect(element).toHaveStyleRule("margin", "0 auto");
   });
 });
