a65e518280962140ace8e6023da0acd9c2e78927
josuenm <direct.josue@gmail.com>
2022-11-12 20:17:49 -0300

    style: formatting

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,28 +1,25 @@
 import { Container } from "./styles";
 
-
 interface ButtonProps {
-    title: string;
-    type?: "button" | "submit";
-    outline?: boolean;
-    onClick?: () => void;
+  title: string;
+  type?: "button" | "submit";
+  outline?: boolean;
+  onClick?: () => void;
 }
 
-
-
-
-export function Button({ 
-    title, 
-    type = "button", 
-    outline = false, 
-    ...rest
+export function Button({
+  title,
+  type = "button",
+  outline = false,
+  ...rest
 }: ButtonProps) {
-
-
-
-    return (
-        <Container type={type} className={outline?'outline':'painted'} {...rest}>
-            {title}
-        </Container>
-    )
+  return (
+    <Container
+      type={type}
+      className={outline ? "outline" : "painted"}
+      {...rest}
+    >
+      {title}
+    </Container>
+  );
 }
