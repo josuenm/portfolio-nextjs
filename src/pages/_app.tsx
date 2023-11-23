fca5c1dd7639d17949e0ce6230ca7f92fdd84020
josuenm <direct.josue@gmail.com>
2023-11-23 16:10:16 -0300

    feat: informação de quem fez o projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { disableReactDevTools } from "src/utils/devTools";
 
+// Feito por Josué Mendonça <direct.josue@gmail.com>
+
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if (process.env.NODE_ENV === "production") disableReactDevTools();
