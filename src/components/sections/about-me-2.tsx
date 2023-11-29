a42ffcb4090478cc91c413b0115324437057094f
josuenm <direct.josue@gmail.com>
2023-11-29 16:12:35 -0300

    feat: atualização do about me2

--- src/components/sections/about-me-2.tsx
+++ src/components/sections/about-me-2.tsx
@@ -4,11 +4,10 @@ export default function AboutMe2() {
   return (
     <SafeArea className="l-stack-center min-h-[500px] my-32" id="about">
       <p className="text-4xl md:text-5xl text-neutral-400 scroll-animate">
-        Agora estou desenvolvendo usando <span className="text-white">PJ</span>{" "}
-        da empresa <span className="text-white">Tech Flow Solutions</span> junto
-        com o co-fundador da <span className="text-white">ValidaPix</span>. A{" "}
-        <span className="text-white">Tech Flow Solutions</span> é um Software
-        House que entrega sistemas de ótima qualidade
+        Agora os contratos de projetos vão ser feitos pela empresa{" "}
+        <span className="text-white">Tech Flow Solutions</span> que foi criada
+        com o propósito de entregar sistemas seguros, robustos e com ótima
+        performance
       </p>
     </SafeArea>
   );
