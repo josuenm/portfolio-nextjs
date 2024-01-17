20a9c6fc39913a41d1f1105a2ce9c8970212472c
josuenm <direct.josue@gmail.com>
2024-01-17 15:16:47 -0300

    fix: espaçamento adicionado na palavra

--- src/components/sections/about-me-2.tsx
+++ src/components/sections/about-me-2.tsx
@@ -4,7 +4,7 @@ export default function AboutMe2() {
   return (
     <SafeArea className="l-stack-center min-h-[500px] my-32" id="about">
       <p className="text-4xl md:text-5xl text-neutral-400 scroll-animate">
-        Agora os contratos de projetos vão ser feitos pela
+        Agora os contratos de projetos vão ser feitos pela{" "}
         <span className="text-white">Workana</span> para realizar projetos de
         freelancer
       </p>
